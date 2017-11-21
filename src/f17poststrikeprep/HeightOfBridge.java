862e794a62b3cc552ff36a952e31937a88621ec0
JaretWright <hkkA223>
2017-11-21 13:09:44 -0500

    Initial upload

--- src/f17poststrikeprep/HeightOfBridge.java
+++ src/f17poststrikeprep/HeightOfBridge.java
@@ -11,15 +11,33 @@ public class HeightOfBridge {
 
     public static void main(String[] args)
     {
-        System.out.printf("How many seconds did it take to get to the ground?: ");
+       /* System.out.printf("How many seconds did it take to get to the ground?: ");
         Scanner keyboard = new Scanner(System.in);
         double seconds = keyboard.nextDouble();
         
         System.out.printf("Based on %.2f seconds to fall, the bridge is %.2f m tall%n",
                                     seconds, calcHeight(seconds));
         System.out.printf("A brick would have been travelling at %.2f m/s when it hit the ground%n", calcSpeed(seconds));
-        System.out.printf("Which is %.1f KPH %n", convertToKPH(calcSpeed(seconds)));
+        System.out.printf("Which is %.1f KPH %n", convertToKPH(calcSpeed(seconds)));*/
+        
+        displayChartOfHeights();
+        
     }
+    
+    
+    public static void displayChartOfHeights()
+    {
+        System.out.printf("%n%n%-6s %-10s %-15s %-15s%n", "Time", "Height(m)", "m/s at impact", "KPH at impact");
+        for (int time=1; time<=5; time++)
+        {
+            System.out.printf("%3d %10.1f %13.1f %15.1f%n", time,
+                                                          calcHeight(time),
+                                                          calcSpeed(time),
+                                                          convertToKPH(calcSpeed(time)));
+        }
+    }
+    
+    
     
     /**
      * Calculate the height of an object based on the # of seconds it takes for
